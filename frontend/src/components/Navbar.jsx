c6931b2762e261fdf8957518873eae0d71e170de
Thanu <thanutheiven@gmail.com>
2026-03-15 05:53:52 +0100

    Responsive Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,78 @@
-import React from 'react'
-import '../index.css'
-import {Link} from '@tanstack/react-router'
+import React, { useState } from "react";
+import { Link } from "@tanstack/react-router";
+
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div>
-      <div className="navBar flex items-center justify-between p-4">
+    <div className="navBar p-4 bg-black text-white">
+      <div className="flex items-center justify-between">
         <span className="font-bold text-xl">CineConnect</span>
-        <ul className="hidden md:flex list-none gap-8">
-          <li className="cursor-pointer">Series</li>
-         <Link to="/film"><li>Films</li></Link> 
+
+        <ul className="hidden md:flex list-none gap-8 text-white">
+          <Link to="/film"><li>Films-Series</li></Link>
           <li className="cursor-pointer">Documentaires</li>
           <li className="cursor-pointer">FAQ</li>
           <li className="cursor-pointer">Aides</li>
         </ul>
 
-        <div className="buttonsNav flex gap-2">
-         <Link to="/login"> <button className="bg-white text-black p-2 rounded border-2 p-2 flex items-center"><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M14 19.2857L15.8 21L20 17M16.5 14.4018C16.2052 14.2315 15.8784 14.1098 15.5303 14.0472C15.4548 14.0337 15.3748 14.024 15.2842 14.0171C15.059 14 14.9464 13.9915 14.7961 14.0027C14.6399 14.0143 14.5527 14.0297 14.4019 14.0723C14.2569 14.1132 13.9957 14.2315 13.4732 14.4682C12.7191 14.8098 11.8817 15 11 15C10.1183 15 9.28093 14.8098 8.52682 14.4682C8.00429 14.2315 7.74302 14.1131 7.59797 14.0722C7.4472 14.0297 7.35983 14.0143 7.20361 14.0026C7.05331 13.9914 6.94079 14 6.71575 14.0172C6.6237 14.0242 6.5425 14.0341 6.46558 14.048C5.23442 14.2709 4.27087 15.2344 4.04798 16.4656C4 16.7306 4 17.0485 4 17.6841V19.4C4 19.9601 4 20.2401 4.10899 20.454C4.20487 20.6422 4.35785 20.7951 4.54601 20.891C4.75992 21 5.03995 21 5.6 21H10.5M15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>S'identifier</button></Link>
+        <div className="hidden md:flex gap-2">
+          <Link to="/login">
+            <button className="bg-white text-black p-2 rounded border-2">
+              S'identifier
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-white text-black p-2 rounded border-2">
+              S'inscrire
+            </button>
+          </Link>
+        </div>
 
-          <Link to = "/signin"><button className="flex items-center bg-white text-black p-2 rounded border-2 p-2 "><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 18L14 18M17 15V21M7.68213 14C8.63244 14.6318 9.77319 15 10.9999 15C11.7012 15 12.3744 14.8797 13 14.6586M10.5 21H5.6C5.03995 21 4.75992 21 4.54601 20.891C4.35785 20.7951 4.20487 20.6422 4.10899 20.454C4 20.2401 4 19.9601 4 19.4V17C4 15.3431 5.34315 14 7 14H7.5M15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>S'inscrire</button></Link>
+        {/* Burger button mobile */}
+        <button
+          className="md:hidden text-2xl"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          ☰
+        </button>
+      </div>
+
+  
+     <div
+  className={`fixed top-0 right-0 h-full w-64 text-white transform transition-transform duration-300 ${
+    menuOpen ? "translate-x-0" : "translate-x-full"
+  }`}
+  style={{ backgroundColor: "#1f2937", zIndex: 1000 }} 
+>
+    <div className="flex justify-between items-center p-4 border-b border-gray-700">
+      <span className="font-bold text-lg">Menu</span>
+      <button className="text-2xl" onClick={() => setMenuOpen(false)}> ✕ </button>
+        </div>
+
+        <ul className="flex flex-col gap-6 p-6">
+          <Link to="/film"><li className="hover:text-gray-300 cursor-pointer">Films-Series</li></Link>
+          <li className="hover:text-gray-300 cursor-pointer">Documentaires</li>
+          <li className="hover:text-gray-300 cursor-pointer">FAQ</li>
+          <li className="hover:text-gray-300 cursor-pointer">Aides</li>
+        </ul>
+
+        <div className="flex flex-col gap-3 p-6">
+          <Link to="/login">
+            <button className="bg-gray-700 text-white p-2 rounded border-2 w-full hover:bg-gray-600">
+              S'identifier
+            </button>
+          </Link>
+
+          <Link to="/signup">
+            <button className="bg-gray-700 text-white p-2 rounded border-2 w-full hover:bg-gray-600">
+              S'inscrire
+            </button>
+          </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
