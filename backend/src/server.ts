0b96cf8416f9085438d02b1eb86a4fc48690914c
MarcheschiThomas <marcheschi.thomas@gmail.com>
2026-03-23 09:14:55 +0100

    Ajout de swagger

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes/router";
+import { swaggerSpec } from "./config/swagger";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
@@ -15,8 +17,18 @@ app.use(
 );
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// Spec JSON brut (optionnel, utile pour Postman)
+app.get("/api-docs.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
 app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Backend CineConnect démarré sur http://localhost:${PORT}`);
+  console.log(
+    `Documentation API disponible sur http://localhost:${PORT}/api-docs`,
+  );
 });
